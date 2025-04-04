4ecd55751ef510dbf5e4d29f166e389d72ac48b1
PRPMichaelT <mike@paperrollsplus.com>
2025-04-04 00:05:25 -0500

    Update shipstation_webhook_v2.py

--- shipstation_webhook_v2.py
+++ shipstation_webhook_v2.py
@@ -46,4 +46,4 @@ def push_to_shipstation():
         return jsonify({"status": "error", "message": response.text}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
